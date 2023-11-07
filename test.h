e83ced43b018abdb7a468684eaed0520203fd2bc
fcchbjm <148618370+fcchbjm@users.noreply.github.com>
2023-11-08 00:30:25 +0800

    Create test.h

---
+++ test.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef __TEST_H__//开头ifndef-if not define-如果没有定义过test.h，没有定义为真，定义过为假，可以防止多次定义
+#define __TEST_H__//那么定义test.h，下面为定义部分
+
+int Add(int, int);//函数的声明
+
+#endif // !__TEST_H__
